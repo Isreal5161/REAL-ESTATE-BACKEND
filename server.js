eebc3147d22ebc7f399c54d66eace674895b21f1
Isreal5161 <diatecktechnology@gmail.com>
2025-09-22 19:22:14 +0100

    your message here

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 // server.js
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
