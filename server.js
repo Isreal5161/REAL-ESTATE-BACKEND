ae15d795a86e84f6483ae6eabf5c938e2a980811
Isreal5161 <diatecktechnology@gmail.com>
2025-09-22 19:04:37 +0100

    your message here

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-const listingRoutes = require("./models/Routes/listingRoutes"); // matches exact casing
+const listingRoutes = require("./models/routes/listingRoutes"); // matches exact casing
 app.use("/api/listings", listingRoutes);
 
 // MongoDB Connection
